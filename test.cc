b6b3fb2ac91e1fe06e894bbcde8ff2fd7c341e4e
Matt Langford <matthew.langford95@gmail.com>
2021-03-09 15:50:21 -0500

    It's working, but I want to make sure the rendering is correct

--- test.cc
+++ test.cc
@@ -121,18 +121,19 @@ size_t size_in_bytes(const std::vector<Data>& vec) {
 
 class CatenarySolver {
 public:
-    CatenarySolver(Eigen::Vector2f start, Eigen::Vector2f end, float length) {
+    CatenarySolver() {}
+
+    void reset(Eigen::Vector2f start, Eigen::Vector2f end, float length) {
         start_ = start;
         diff_.x() = end.x() - start.x();
         diff_.y() = end.y() - start.y();
+        beta_ = 10.0;
         length_ = length;
+        x_offset_ = 0;
+        y_offset_ = 0;
     }
 
-    double f(double x) const { return alpha_ * std::cosh((x - x_offset_) / alpha_) + y_offset_; }
-
-    constexpr static double sq(double in) { return in * in; }
-
-    bool solve(double b = 10, double tol = 1E-3, size_t max_iter = 100) {
+    bool solve(double tol = 1E-3, size_t max_iter = 100) {
         size_t iter = 0;
 
         // Function we're optimizing which relates the free parameter (b) to the size of the opening we need.
@@ -149,13 +150,13 @@ public:
 
         // Newton iteration
         for (; iter < max_iter; ++iter) {
-            float y_val = y(b);
+            float y_val = y(beta_);
             if (abs(y_val) < tol) break;
-            b -= y_val / dy(b);
+            beta_ -= y_val / dy(beta_);
         }
 
         // Since b = sqrt(h / a), we can solve easily for a
-        alpha_ = diff_.x() * sq(b);
+        alpha_ = diff_.x() * sq(beta_);
         x_offset_ = 0.5 * (diff_.x() + alpha_ * std::log((length_ - diff_.y()) / (length_ + diff_.y())));
         y_offset_ = -f(0);
         return iter < max_iter;
@@ -173,15 +174,49 @@ public:
         return result;
     }
 
-    float get_alpha() const { return alpha_; }
+    double& length() { return length_; }
 
 private:
+    double f(double x) const { return alpha_ * std::cosh((x - x_offset_) / alpha_) + y_offset_; }
+    constexpr static double sq(double in) { return in * in; }
+
     Eigen::Vector2f start_;
     Eigen::Vector2d diff_;
     double length_;
+    float beta_;
     float alpha_;
     float y_offset_ = 0;
     float x_offset_ = 0;
+};
+
+struct CatenaryObject {
+    static constexpr size_t kNumSteps = 3;
+    std::vector<Eigen::Vector2f> calculate_points() {
+        solver.reset(start, end, std::max(solver.length(), min_length()));
+        if (!solver.solve()) throw std::runtime_error("Unable to update CatenaryObject, did not converge.");
+
+        auto points = solver.trace(kNumSteps);
+        // points.emplace_back(end);
+        return points;
+    }
+
+    double min_length() const { return 1.01 * (end - start).norm(); }
+
+    void draw_lines(engine::VertexArrayObject& vao) const {
+        const void* indices = reinterpret_cast<void*>(sizeof(unsigned int) * start_element_index);
+        gl_check_with_vao(vao, glDrawElements, GL_TRIANGLES, 3 * 2 * kNumSteps, GL_UNSIGNED_INT, indices);
+    }
+    void draw_points(engine::VertexArrayObject& vao) const {
+        gl_check_with_vao(vao, glDrawArrays, GL_POINTS, start_element_index, 1);
+        gl_check_with_vao(vao, glDrawArrays, GL_POINTS, start_element_index + (kNumSteps - 1), 1);
+    }
+
+    Eigen::Vector2f start;
+    Eigen::Vector2f end;
+    CatenarySolver solver;
+    size_t start_vertex_index;
+    size_t start_element_index;
+    bool needs_update;
 };
 
 class TestObjectManager final : public engine::AbstractObjectManager {
@@ -191,7 +226,18 @@ public:
           point_shader_(vertex_shader_text, fragment_shader_text, point_geometry_shader_text) {}
     virtual ~TestObjectManager() = default;
 
-    static constexpr size_t kNumSteps = 64;
+    void spawn_object(Eigen::Vector2f start, Eigen::Vector2f end) {
+        objects_.emplace_back(CatenaryObject{start, end, {}, vbo_.size(), ebo_.size(), true});
+        vbo_.resize(vbo_.size() + 2 * CatenaryObject::kNumSteps);
+
+        size_t element_index = ebo_.size();
+        auto elements = ebo_.batched_updater();
+        for (size_t i = 0; i < CatenaryObject::kNumSteps - 1; ++i) {
+            elements.push_back(element_index + i);
+            elements.push_back(element_index + i + 1);
+            elements.push_back(element_index + i + 2);
+        }
+    }
 
     void init() override {
         shader_.init();
@@ -200,95 +246,69 @@ public:
         sfw_ = glGetUniformLocation(shader_.get_program_id(), "screen_from_world");
         engine::throw_on_gl_error("glGetUniformLocation");
 
-        reset();
-
         vao_.init();
         vbo_.init(GL_ARRAY_BUFFER, 0, 2, vao_);
         ebo_.init(GL_ELEMENT_ARRAY_BUFFER, vao_);
 
-        auto elements = ebo_.batched_updater();
-        for (size_t i = 0; i < kNumSteps - 1; ++i) {
-            elements.push_back(i);
-            elements.push_back(i + 1);
-            elements.push_back(i + 2);
-        }
+        spawn_object({100, 100}, {500, 500});
     }
 
     void render(const Eigen::Matrix3f& screen_from_world) override {
+        if (ebo_.size() == 0) return;
+
         shader_.activate();
         gl_check(glUniformMatrix3fv, sfw_, 1, GL_FALSE, screen_from_world.data());
-        gl_check_with_vao(vao_, glDrawElements, GL_TRIANGLES, ebo_.size(), GL_UNSIGNED_INT, (void*)0);
+
+        for (const auto& object : objects_) object.draw_lines(vao_);
 
         point_shader_.activate();
         gl_check(glUniformMatrix3fv, sfw_, 1, GL_FALSE, screen_from_world.data());
-        gl_check_with_vao(vao_, glDrawArrays, GL_POINTS, 0, 1);
-        gl_check_with_vao(vao_, glDrawArrays, GL_POINTS, kNumSteps, 1);
+
+        for (const auto& object : objects_) object.draw_points(vao_);
     }
 
     void update(float) override {
-        if (!needs_update_) return;
-        needs_update_ = false;
-
-        length_ = std::max(min_length(), length_);
-        CatenarySolver solver{start_, end_, length_};
-        if (!solver.solve(alpha_)) {
-            throw std::runtime_error("CatenarySolver unable to converge!");
-        }
-        alpha_ = solver.get_alpha();
-        auto points = solver.trace(kNumSteps);
-
         auto vertices = vbo_.batched_updater();
-        vertices.resize(2 * (points.size() + 1));
 
-        constexpr size_t stride = 2;
-        for (size_t i = 0; i < points.size(); ++i) {
-            const Eigen::Vector2f& point = points[i];
+        for (auto& object : objects_) {
+            if (!object.needs_update) continue;
 
-            size_t el = 0;
-            vertices[stride * i + el++] = point.x();
-            vertices[stride * i + el++] = point.y();
+            object.needs_update = false;
+            size_t index = object.start_vertex_index;
+            for (auto point : object.calculate_points()) {
+                vertices[index++] = point.x();
+                vertices[index++] = point.y();
+            }
         }
-
-        size_t el = 0;
-        vertices[stride * points.size() + el++] = end_.x();
-        vertices[stride * points.size() + el++] = end_.y();
     }
 
     void handle_mouse_event(const engine::MouseEvent& event) override {
-        if (point_ && event.held()) {
+        if (selected_ && point_ && event.held()) {
             *point_ = event.mouse_position;
-            needs_update_ = true;
+            selected_->needs_update = true;
         } else if (event.pressed()) {
-            constexpr float kClickRadius = 10;
-            if ((event.mouse_position - start_).squaredNorm() < kClickRadius) {
-                point_ = &start_;
-            } else if ((event.mouse_position - end_).squaredNorm() < kClickRadius) {
-                point_ = &end_;
+            for (auto& object : objects_) {
+                constexpr float kClickRadius = 10;
+                if ((event.mouse_position - object.start).squaredNorm() < kClickRadius) {
+                    point_ = &object.start;
+                    selected_ = &object;
+                } else if ((event.mouse_position - object.end).squaredNorm() < kClickRadius) {
+                    point_ = &object.end;
+                    selected_ = &object;
+                }
             }
         } else {
             point_ = nullptr;
+            selected_ = nullptr;
         }
     }
 
-    float min_length() const { return 1.01 * (end_ - start_).norm(); }
+    void handle_keyboard_event(const engine::KeyboardEvent&) override {}
 
-    void reset() {
-        start_ = {100, 200};
-        end_ = {200, 300};
-        length_ = min_length();
-    }
-
-    void handle_keyboard_event(const engine::KeyboardEvent& event) override {
-        if (event.space == true) {
-            reset();
-        }
-    }
-
+    CatenaryObject* selected_;
     Eigen::Vector2f* point_;
-    float length_ = 0.0;
-    Eigen::Vector2f start_;
-    Eigen::Vector2f end_;
-    float alpha_ = 10.0;
+
+    std::vector<CatenaryObject> objects_;
 
     bool needs_update_ = true;
 
