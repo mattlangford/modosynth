9a7206378a25c6ec09ea72a576d98cb342c24584
Matt Langford <matthew.langford95@gmail.com>
2021-03-15 16:55:52 -0400

    Get the stream up and running

--- synth/bridge.hh
+++ synth/bridge.hh
@@ -19,48 +19,59 @@ struct Identifier {
 
 class Stream {
 public:
-    Stream()
-        : end_time_(0), raw_samples_(100, true), output_(std::make_unique<ThreadSafeBuffer>(Samples::kSampleRate)) {}
+    Stream() : raw_samples_(100, true), output_(std::make_unique<ThreadSafeBuffer>(Samples::kSampleRate)) {}
 
     void add_samples(const std::chrono::nanoseconds& timestamp, const Samples& samples) {
-        if (timestamp < end_time_) {
+        if (timestamp <= end_time_) {
             // Update the element instead of adding a new one. Average the current sample with the new one.
             raw_samples_[index_of_timestamp(timestamp)].combine(0.5, samples.samples, 0.5);
             return;
         }
 
-        end_time_ = timestamp;
+        end_time_ = timestamp;  // store the timestamp of the start of this batch
         raw_samples_.push(samples);
     }
 
-    void flush_samples(const std::chrono::nanoseconds end_time) {
-        for (size_t i = 0; i <= index_of_timestamp(end_time); ++i) {
+    void flush_samples(const std::chrono::nanoseconds& amount_to_flush) {
+        for (size_t i = 0; i < static_cast<size_t>(amount_to_flush / Samples::kBatchIncrement); ++i) {
             auto element = raw_samples_.pop();
-            if (element) {
-                for (auto sample : element->samples) {
-                    output_->push(sample);
-                }
-            } else {
-                throttled(1.0, "Warning: Stream::flush_samples() with end time past end of buffer. Padding with 0s.");
-                for (size_t el = 0; el < Samples::kBatchSize; ++el) output_->push(0);
+            std::cout << "Popping : " << i << " value? " << element.has_value() << "\n";
+            if (!element) {
+                default_flush();
+                continue;
+            }
+            for (auto sample : element->samples) {
+                output_->push(sample);
             }
         }
     }
 
     size_t index_of_timestamp(const std::chrono::nanoseconds& timestamp) {
-        // 0 will be the oldest entry, raw_samples_.size() - 1 will be the latest
-        std::chrono::nanoseconds start_time = end_time_ - Samples::kBatchIncrement * (raw_samples_.size() - 1);
-        if (timestamp < start_time) return 0;
-        std::cout << "start_time: " << start_time.count() << " end_time_: " << end_time_.count()
+        if (raw_samples_.empty() || !end_time_) throw std::runtime_error("Can't get index without adding samples.");
+
+        // The start time of the oldest element in the buffer
+        std::chrono::nanoseconds start_time = *end_time_ - Samples::kBatchIncrement * (raw_samples_.size() - 1);
+
+        std::cout << "start_time: " << start_time.count() << " end_time_: " << end_time_->count()
                   << ", t=" << timestamp.count() << "\n";
+
+        if (timestamp < start_time) throw std::runtime_error("Asking for timestamp before start of buffer.");
+
+        // 0 will be the oldest entry, raw_samples_.size() - 1 will be the latest
         return (timestamp - start_time) / Samples::kBatchIncrement;
     }
 
     ThreadSafeBuffer& buffer() { return *output_; }
 
 private:
+    void default_flush() {
+        throttled(1.0, "Warning: Stream::flush_samples() with end time past end of buffer. Padding with 0s.");
+        for (size_t el = 0; el < Samples::kBatchSize; ++el) output_->push(0);
+    }
+
+private:
     // The most recent samples timestamp (other timestamps are calculated with respect to this
-    std::chrono::nanoseconds end_time_;
+    std::optional<std::chrono::nanoseconds> end_time_;
     Buffer<Samples> raw_samples_;
 
     // Kept as a pointer so that the address is always valid
--- synth/test/stream_test.cc
+++ synth/test/stream_test.cc
@@ -8,19 +8,27 @@ TEST(Stream, basic_flush) {
     Stream s;
 
     EXPECT_EQ(s.buffer().size(), 0);
-    EXPECT_EQ(s.index_of_timestamp(std::chrono::nanoseconds(0)), 0);
+    EXPECT_THROW(s.index_of_timestamp(std::chrono::seconds(0)), std::runtime_error);
 
     auto inc = Samples::kBatchIncrement;
 
     s.add_samples(0 * inc, Samples{100});
     EXPECT_EQ(s.index_of_timestamp(0 * inc), 0);
+
+    // Queries too far in the past should fail
+    EXPECT_THROW(s.index_of_timestamp(std::chrono::seconds(-10)), std::runtime_error);
+
     s.add_samples(1 * inc, Samples{200});
     EXPECT_EQ(s.index_of_timestamp(0 * inc), 0);
     EXPECT_EQ(s.index_of_timestamp(1 * inc), 1);
-    s.add_samples(2 * inc, Samples{300});
+    s.add_samples(2 * inc, Samples{1000});
     EXPECT_EQ(s.index_of_timestamp(0 * inc), 0);
     EXPECT_EQ(s.index_of_timestamp(1 * inc), 1);
     EXPECT_EQ(s.index_of_timestamp(2 * inc), 2);
+
+    // Flushing zero shouldn't produce any data
+    s.flush_samples(std::chrono::nanoseconds{0});
+    ASSERT_EQ(s.buffer().size(), 0);
 
     // Flush samples, this should flush the first and second set of samples added above
     s.flush_samples(2 * inc);
@@ -33,5 +41,18 @@ TEST(Stream, basic_flush) {
         ASSERT_TRUE(s.buffer().pop(value)) << "i=" << i;
         EXPECT_EQ(value, expected);
     }
+    EXPECT_EQ(s.buffer().size(), 0);
+
+    s.add_samples(2 * inc, Samples{2000});  // overwrite the data at t=2*inc
+    s.flush_samples(inc);                   // flush the last sample
+    ASSERT_EQ(s.buffer().size(), Samples::kBatchSize);
+    for (size_t i = 0; i < Samples::kBatchSize; ++i) {
+        float value;
+        ASSERT_TRUE(s.buffer().pop(value)) << "i=" << i;
+        EXPECT_EQ(value, 1500.0);  // average of 1000 and 2000
+    }
+
+    // We can't add samples in the past
+    EXPECT_THROW(s.add_samples(1 * inc, Samples{2000}), std::runtime_error);
 }
 }  // namespace synth
