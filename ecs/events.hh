f488d7183ff8159460a7abe9d7a04dd9f890bd4d
Matt Langford <matthew.langford95@gmail.com>
2021-03-18 14:12:26 -0400

    Setup undo strctures

--- ecs/events.hh
+++ ecs/events.hh
@@ -1,6 +1,8 @@
 #pragma once
 #include <functional>
+#include <stack>
 #include <tuple>
+#include <variant>
 #include <vector>
 
 #include "ecs/utils.hh"
@@ -17,19 +19,40 @@ public:
     void add_handler(Handler<E> handler) {
         std::get<kIndexOf<E>>(handlers_).push_back(std::move(handler));
     }
+    template <typename E>
+    void add_undo_handler(Handler<E> handler) {
+        std::get<kIndexOf<E>>(undo_handlers_).push_back(std::move(handler));
+    }
 
     template <typename E>
-    void trigger(const E& event) {
+    void trigger(E event = {}) {
         for (auto& handler : std::get<kIndexOf<E>>(handlers_)) {
             handler(event);
         }
+        undo_.push(std::move(event));
+    }
+
+    void undo() {
+        if (undo_.empty()) return;
+        std::visit([this](auto& event) { this->trigger_undo(std::move(event)); }, undo_.top());
+        undo_.pop();
     }
 
 private:
     template <typename E>
     static constexpr size_t kIndexOf = Index<E, Event...>::value;
 
+    template <typename E>
+    void trigger_undo(E event) {
+        for (auto& handler : std::get<kIndexOf<E>>(undo_handlers_)) {
+            handler(event);
+        }
+    }
+
 private:
+    std::stack<std::variant<Event...>> undo_;
+
     std::tuple<std::vector<Handler<Event>>...> handlers_;
+    std::tuple<std::vector<Handler<Event>>...> undo_handlers_;
 };
 }  // namespace ecs
