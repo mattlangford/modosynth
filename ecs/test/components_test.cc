2912e5e0d6a5719f79bcc6fc7b3178f1ec907715
Matt Langford <matthew.langford95@gmail.com>
2021-03-18 11:50:01 -0400

    despawn test

--- ecs/test/components_test.cc
+++ ecs/test/components_test.cc
@@ -35,6 +35,46 @@ TEST(ComponentManager, spawn_and_add) {
 // #############################################################################
 //
 
+TEST(ComponentManager, despawn) {
+    MyManager manager;
+    manager.spawn_with<TestComponentA>();
+    auto entity_ab = manager.spawn_with<TestComponentA, TestComponentB>();
+    auto entity_abc = manager.spawn_with<TestComponentA, TestComponentB, TestComponentC>();
+
+    {
+        size_t count = 0;
+        manager.run_system<TestComponentA>(
+            [&](const Entity&, TestComponentA& component) { component.value = count++; });
+        EXPECT_EQ(count, 3);
+    }
+
+    manager.despawn(entity_ab);
+
+    {
+        size_t count = 0;
+        manager.run_system<TestComponentA>([&](const Entity&, const TestComponentA&) { count++; });
+        EXPECT_EQ(count, 2);
+    }
+
+    // Make sure the remaining elements are still correct
+    {
+        const TestComponentA* a = manager.get_component<TestComponentA>(entity_abc);
+        ASSERT_NE(a, nullptr);
+        EXPECT_EQ(a->value, 2);
+    }
+
+    // Now back to 3
+    manager.spawn_with<TestComponentA, TestComponentC>();
+    {
+        size_t count = 0;
+        manager.run_system<TestComponentA>([&](const Entity&, const TestComponentA&) { count++; });
+        EXPECT_EQ(count, 3);
+    }
+}
+//
+// #############################################################################
+//
+
 TEST(ComponentManager, run_system) {
     MyManager manager;
     manager.spawn_with<TestComponentA>();
@@ -61,5 +101,24 @@ TEST(ComponentManager, run_system) {
 
     EXPECT_EQ(a->value, 2);
     EXPECT_EQ(c->set, true);
+}
+
+//
+// #############################################################################
+//
+
+TEST(ComponentManager, dynmamic) {
+    MyManager manager;
+
+    // No component
+    auto entity = manager.spawn_with();
+    manager.add_component_by_index(entity, 0);  // A
+
+    TestComponentA* a = manager.get_component<TestComponentA>(entity);
+    ASSERT_NE(a, nullptr);
+    EXPECT_EQ(a->value, 0);  // default constructed
+
+    manager.run_system<TestComponentA>([](const Entity&, TestComponentA& a) { a.value = 100; });
+    EXPECT_EQ(a->value, 0);  // default constructed
 }
 }  // namespace ecs
