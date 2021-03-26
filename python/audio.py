8754a95a3f4bef2729c20173c4112243c00fdfe6
Matt Langford <matthew.langford95@gmail.com>
2021-03-26 00:01:06 -0400

    Add BiQuad filter python

--- python/audio.py
+++ python/audio.py
@@ -16,7 +16,11 @@ class Signal(object):
         self.data = []
         self.phase = 0
 
-    def update_samples(self, f):
+    def sum(self, signal):
+        for (batch, rhs_batch) in zip(self.data, signal.data):
+            batch += rhs_batch
+
+    def populate_samples(self, f):
         phase_inc = 2 * np.pi * f / SampleRate
         x = np.arange(0, BatchSize, phase_inc)
         self.data.append(np.sin(self.phase + x))
@@ -31,14 +35,23 @@ class Signal(object):
         x = np.arange(0, len(full_data)) / SampleTime
 
         plt.plot(x, np.array(full_data))
-        plt.show()
+
+    def fft(self, max_freq):
+        full_data = []
+        for d in self.data:
+            full_data.extend(d.tolist())
+
+        fft_wave = np.fft.rfft(full_data)
+        fft_freq = np.fft.rfftfreq(len(full_data), d=1.0/SampleRate)
+
+        plt.plot(fft_freq[:max_freq], np.array(fft_wave)[:max_freq])
 
     def write(self, name):
         full_data = []
         for d in self.data:
             full_data.extend(d.tolist())
 
-        f = wave.open("/tmp/test.wav", 'wb')
+        f = wave.open(name, 'wb')
         f.setnchannels(1)
         f.setsampwidth(2)
         f.setframerate(SampleRate)
@@ -51,23 +64,82 @@ class Signal(object):
 
         f.close()
 
+def intermediate(fs, f0, db_gain, s):
+    A = 10. ** (db_gain / 40.)
+    w = 2 * np.pi * f0 / fs
+    alpha = 0.5 * np.sin(w) * np.sqrt((A + 1 / A) * (1 / s - s) + 2)
+    print (f"Q: {1 / np.sqrt((A + 1 / A) * (1 / s - s) + 2)}")
+    return w, alpha
+
+# https://webaudio.github.io/Audio-EQ-Cookbook/audio-eq-cookbook.html for the coeff calculations
+class BiQuadFilter(object):
+    def __init__(self, coeff):
+        print (f"BiQuadFilter Coefficients: {coeff}")
+        self.coeff = coeff
+
+    @staticmethod
+    def lpf(fs, f0, db_gain, s):
+        w, alpha = intermediate(fs, f0, db_gain, s)
+        b0 = (1 - np.cos(w)) * 0.5
+        b1 = 2 * b0
+        b2 = b0
+        a0 = 1 + alpha
+        a1 = -2 * np.cos(w)
+        a2 = 1 - alpha
+        return BiQuadFilter([b0 / a0, b1 / a0, b2 / a0, a0 / a0, a1 / a0, a2 / a0])
+
+    @staticmethod
+    def hpf(fs, f0, db_gain, s):
+        w, alpha = intermediate(fs, f0, db_gain, s)
+        b0 = (1 + np.cos(w)) * 0.5
+        b1 = -2 * b0
+        b2 = b0
+        a0 = 1 + alpha
+        a1 = -2 * np.cos(w)
+        a2 = 1 - alpha
+        return BiQuadFilter([b0 / a0, b1 / a0, b2 / a0, a0 / a0, a1 / a0, a2 / a0])
+
+    def process(self, signal):
+        xn_1 = 0
+        xn_2 = 0
+        yn_1 = 0
+        yn_2 = 0
+
+        b0, b1, b2, a0, a1, a2 = np.copy(self.coeff)
+
+        output = Signal()
+        for batch in signal.data:
+            output_batch = []
+            for sample in batch:
+                xn = sample
+                yn = b0 * xn + b1 * xn_1 + b2 * xn_2 - a1 * yn_1 - a2 * yn_2
+
+                output_batch.append(yn)
+
+                xn_2 = xn_1
+                xn_1 = xn
+
+                yn_2 = yn_1
+                yn_1 = yn
+
+            output.data.append(np.array(output_batch))
+
+        return output
 
 s = Signal()
 
-for i in range(10):
-    s.update_samples(440)
+t = np.linspace(0, 1, SampleRate)
+data = np.cos(2 * np.pi * 1000 * t)
+for f in range(10, 1000, 10):
+    data += np.cos(2 * np.pi * f * t)
 
-for i in range(10):
-    s.update_samples(1000)
+s.data.append(data)
 
-# for i in range(200):
-#     s.update_samples(1000, (300 + i) * BatchTime)
-# for i in range(100):
-#     s.update_samples(5500, (400 + i) * BatchTime)
+f = BiQuadFilter.hpf(SampleRate, 600, 6, 0.5)
+s.fft(2000)
+s = f.process(s)
+
 s.write("/tmp/out.wav")
-s.plot()
+s.fft(2000)
+plt.show()
 
-# for i in range(10):
-#     s.update_samples(500, (10 + i) * BatchTime)
-
-#s.plot()
